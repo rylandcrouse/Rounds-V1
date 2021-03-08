e8a4bf311f6b72f5728421f97fd6f818526f7f5a
Ryland Crouse <rylandcrouse@gmail.com>
2021-03-07 21:23:20 -0500

    handle join with button

--- client/src/views/home/states/joinFocused.jsx
+++ client/src/views/home/states/joinFocused.jsx
@@ -48,7 +48,7 @@ const JoinFocused = observer(({ back }) => {
                 <StyledSVG id="back-btn" xmlns="http://www.w3.org/2000/svg" fill="currentColor" className="bi bi-arrow-left-short" viewBox="0 0 16 16">
                     <path id="back-btn" fillRule="evenodd" d="M12 8a.5.5 0 0 1-.5.5H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5a.5.5 0 0 1 .5.5z" />
                 </StyledSVG>
-                <Button variant='dark' >Join</Button>
+                <Button onClick={() => handleJoin()} variant='dark' >Join</Button>
                 <svg style={{ visibility: "hidden" }} xmlns="http://www.w3.org/2000/svg" width="2.5em" height="2.5em" fill="currentColor" className="bi bi-arrow-left-short" viewBox="0 0 16 16">
                     <path fillRule="evenodd" d="M12 8a.5.5 0 0 1-.5.5H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5a.5.5 0 0 1 .5.5z" />
                 </svg>
