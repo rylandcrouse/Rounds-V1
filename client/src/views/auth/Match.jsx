7384bab07c07ca944a2194b5fec8dd3c824dfca3
Ryland Crouse <rylandcrouse@gmail.com>
2021-03-11 21:29:15 -0500

    succesfull 2fa redirect

--- client/src/views/auth/Match.jsx
+++ client/src/views/auth/Match.jsx
@@ -47,7 +47,7 @@ const Match = observer(() => {
 
     // if success, go to 2FA step
     if (!store.auth.matchEmail) return <Redirect to='/login' />
-    if (store.auth.user) return <Redirect to='/home' />
+    if (store.auth.user) return <Redirect to='/' />
 
 
     return (
