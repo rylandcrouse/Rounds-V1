da2cb7de7718cf7c56f61eb0f98ec02269f3d311
Ryland Crouse <rylandcrouse@gmail.com>
2021-03-11 23:44:23 -0500

    trying herokuenv fix

--- client/src/utils/config.js
+++ client/src/utils/config.js
@@ -2,13 +2,13 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const io = {
-    host: process.env.IO_HOST || process.env.HOST,
-    port: process.env.IO_PORT || process.env.PORT
+    host: process.env.REACT_APP_IO_HOST || process.env.HOST,
+    port: process.env.REACT_APP_IO_PORT || process.env.PORT
 }
 
 const auth = {
-    host: process.env.AUTH_HOST || process.env.HOST,
-    port: process.env.AUTH_PORT || process.env.PORT
+    host: process.env.REACT_APP_AUTH_HOST || process.env.HOST,
+    port: process.env.REACT_APP_AUTH_PORT || process.env.PORT,
 }
 
 const config = {
