52ac70bbcc71f611eeb96d4c18855cb3230d94ff
Ryland Crouse <rmikhailc@gmail.com>
2021-03-12 17:35:42 -0500

    env

--- client/src/utils/config.js
+++ client/src/utils/config.js
@@ -2,15 +2,14 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const io = {
-    host: process.env.REACT_APP_IO_HOST || process.env.REACT_APP_PORT,
+    host: process.env.REACT_APP_IO_HOST || process.env.REACT_APP_HOST,
     port: process.env.REACT_APP_IO_PORT || process.env.REACT_APP_HOST
 }
 
-console.log(io)
 
 const auth = {
-    host: process.env.REACT_APP_AUTH_HOST || process.env.HOST,
-    port: process.env.REACT_APP_AUTH_PORT || process.env.PORT,
+    host: process.env.REACT_APP_AUTH_HOST || process.env.REACT_APP_HOST,
+    port: process.env.REACT_APP_AUTH_PORT || process.env.REACT_APP_PORT,
 }
 
 const config = {
